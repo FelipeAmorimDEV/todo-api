c47eab1698fc859d026b8c2c4c0b60633694e4c9
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 13:14:00 -0300

    ✔  Request POST /todos + Request para rotas que não existe

--- src/server.js
+++ src/server.js
@@ -1,16 +1,9 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+
+const todos = []
 
 const server = http.createServer((req, res) => {
-  const todos = [
-    {
-      id: 1,
-      title: 'Ignite Node',
-      description: 'Terminar o modulo 1',
-      completed_at: null,
-      created_at: new Date(),
-      updated_at: null
-    }
-  ]
   const { method, url } = req
 
   if (method === 'GET' && url === '/todos') {
@@ -18,7 +11,20 @@ const server = http.createServer((req, res) => {
     return res.writeHead(200).end(JSON.stringify(todos))
   }
 
-  return res.end('Resposta da req')
+  if (method === 'POST' && url === '/todos') {
+    todos.push({
+      id: randomUUID(),
+      title: 'Ignite Node',
+      description: 'Terminar o modulo 1',
+      completed_at: null,
+      created_at: new Date(),
+      updated_at: null
+    })
+
+    return res.writeHead(201).end('')
+  }
+
+  return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3334)
