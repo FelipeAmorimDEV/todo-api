4d5223f535df6b1d4aec86f1d234f40afc178d20
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 16:45:12 -0300

    ✔  Rotas abstraidas para um objeto

--- src/server.js
+++ src/server.js
@@ -9,68 +9,104 @@ const database = new Database()
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  await json(req,res)
+  await json(req, res)
 
-  if (method === 'GET' && buildRouterPath('/todos').test(url)) {
-    const routeParams = url.match(buildRouterPath('/todos'))
-    const { query } = routeParams.groups
-    const params = query ? extractSearchParams(query) : null
-   
-    const todos = database.select('todos', params)
-    
-    return res.writeHead(200).end(JSON.stringify(todos))
-  }
+  const routes = [
+    {
+      method: 'GET',
+      path: buildRouterPath('/todos'),
+      handle: (req, res) => {
+        const { search } = req.query
+        const todos = database.select('todos', search ? {
+          title: search,
+          description: search
+        } : null)
 
-  if (method === 'POST' && url === '/todos') {
-    const { title, description } = req.body
+        return res.writeHead(200).end(JSON.stringify(todos))
+      }
+    },
+    {
+      method: 'POST',
+      path: buildRouterPath('/todos'),
+      handle: (req, res) => {
+        const { title, description } = req.body
 
+        if (title === undefined) {
+          const status = { status: 'title is required' }
+          return res.writeHead(400).end(JSON.stringify(status))
+        }
 
-    if (title === undefined  || description === undefined) {
-      return res.writeHead(400).end(JSON.stringify({ status: "request body is invalid" }))
-    }
-    
-    const data = {
-      id: randomUUID(),
-      completed_at: null,
-      created_at: new Date(),
-      updated_at: null,
-      title,
-      description
-    }
+        if (description === undefined) {
+          const status = { status: 'description is required' }
+          return res.writeHead(400).end(JSON.stringify(status))
+        }
 
-    database.insert('todos', data)
-    return res.writeHead(201).end('')
-  }
+        const data = {
+          id: randomUUID(),
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: null,
+          title,
+          description
+        }
+        database.insert('todos', data)
 
-  if (method === 'DELETE' && buildRouterPath('/todos/:id').test(url)) {
-    const routeParams = url.match(buildRouterPath('/todos/:id'))
-    const { id } = routeParams.groups
-    database.delete('todos', id)
+        return res.writeHead(201).end('')
+      }
+    },
+    {
+      method: 'DELETE',
+      path: buildRouterPath('/todos/:id'),
+      handle: (req, res) => {
+        const { id } = req.params
+        database.delete('todos', id)
 
-    return res.writeHead(204).end()
-  }
+        return res.writeHead(204).end('')
+      }
+    },
+    {
+      method: 'PUT',
+      path: buildRouterPath('/todos/:id'),
+      handle: (req, res) => {
+        const { title, description } = req.body
+        const { id } = req.params
 
-  if (method === 'PUT' && buildRouterPath('/todos/:id').test(url)) {
-    const routeParams = url.match(buildRouterPath('/todos/:id'))
-    const { id } = routeParams.groups
-    const { title, description } = req.body
+        if (title === undefined) {
+          const status = { status: 'title is required' }
+          return res.writeHead(400).end(JSON.stringify(status))
+        }
 
-    if (title === undefined  || description === undefined) {
-      return res.writeHead(400).end(JSON.stringify({ status: "request body is invalid" }))
-    }
+        if (description === undefined) {
+          const status = { status: 'description is required' }
+          return res.writeHead(400).end(JSON.stringify(status))
+        }
 
-    database.update('todos', id, { title, description, updated_at: new Date() })
+        database.update('todos', id, { title, description, updated_at: new Date() })
 
-    return res.writeHead(204).end()
-  }
+        return res.writeHead(204).end('')
+      }
+    },
+    {
+      method: 'PATCH',
+      path: buildRouterPath('/todos/:id/complete'),
+      handle: (req, res) => {
+        const { id } = req.params
+        database.update('todos', id, { completed_at: new Date(), updated_at: new Date() })
 
-  if (method === 'PATCH' && buildRouterPath('/todos/:id/complete').test(url)) {
-    const routeParams = url.match(buildRouterPath('/todos/:id'))
-    const { id } = routeParams.groups
-    
-    database.update('todos', id, { completed_at, updated_at: new Date() })
+        return res.writeHead(204).end('')
+      }
+    },
+  ]
 
-    return res.writeHead(204).end()
+  const route = routes.find(route => route.method === method && route.path.test(url))
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+    const { query, ...params } = routeParams.groups
+
+    req.query = query ? extractSearchParams(query) : {}
+    req.params = params
+    return route.handle(req, res)
   }
 
   return res.writeHead(404).end('Not Found')
