1a61a4d2fcaea973b84a1268caa9fac79700ce10
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 15:09:29 -0300

    ✔  Metodo update preserva os dados anterior e espalha os novos valores na row

--- src/database.js
+++ src/database.js
@@ -45,11 +45,10 @@ export default class Database {
   }
 
   update(table, id, data) {
-    const { title, description } = data
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
     if (rowIndex > -1) {
-      this.#database[table][rowIndex] = { ...this.#database[table][rowIndex], title, description }
+      this.#database[table][rowIndex] = { ...this.#database[table][rowIndex], ...data }
       this.#persist()
     }
   }
