8feb5588444c8a01905e7d089fbc3e839f55a148
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 14:43:39 -0300

    ✔  buildRouterPath agora nomeia os recursos

--- src/utils/build-router-path.js
+++ src/utils/build-router-path.js
@@ -2,7 +2,7 @@
 
 export default function buildRouterPath(path)  {
   const routerParameterRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routerParameterRegex, '([a-z0-9\-_]+)')
+  const pathWithParams = path.replaceAll(routerParameterRegex, '(?<$1>[a-z0-9\-_]+)')
   const pathRegex = new RegExp(`^${pathWithParams}`)
 
   return pathRegex
