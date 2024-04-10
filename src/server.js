5ef4c28b746424298fdc11f05bb32886f6fac269
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 12:59:43 -0300

    ✔  Request GET /todos criado

--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  res.end('Servidor criado')
+  const todos = [
+    {
+      id: 1,
+      title: 'Ignite Node',
+      description: 'Terminar o modulo 1',
+      completed_at: null,
+      created_at: new Date(),
+      updated_at: null
+    }
+  ]
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/todos') {
+    res.setHeader('Content-Type', 'application/json')
+    return res.writeHead(200).end(JSON.stringify(todos))
+  }
+
+  return res.end('Resposta da req')
 })
 
 server.listen(3334)
