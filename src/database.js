a97c1fb201007a8fddb7e33053491a60dab787b3
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 13:52:04 -0300

    ✔  Metodos persist implementado na class Database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
 export default class Database {
-  #database = []
+  #database = {}
+
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] || []
@@ -8,11 +16,16 @@ export default class Database {
 
   insert(table, data) {
     const eUmaTabelaExistente = this.#database[table]
+
     if(eUmaTabelaExistente) {
       this.#database[table].push(data)
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
+
+    return data
   }
 }
 
