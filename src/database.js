4cd685c1215dae51b47732a5c53c752d5ef1c841
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-10 13:57:47 -0300

    ✔  Obtendo banco de dados do db.json

--- src/database.js
+++ src/database.js
@@ -4,6 +4,14 @@ const databasePath = new URL('../db.json', import.meta.url)
 export default class Database {
   #database = {}
 
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      }).catch(() => {
+        this.#persist()
+      })
+  }
 
   #persist() {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
